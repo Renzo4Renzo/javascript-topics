4782a1a0355e4cdd77b2d1a275203dc31042292e
Renzo Diaz <renzodiaz554@gmail.com>
2024-04-23 14:38:19 -0500

    Adding Variable Shadowing

--- var-let-const.js
+++ var-let-const.js
@@ -7,13 +7,20 @@
 
   CONST and LET 
     Could be Block-scoped
-      A variable with the same name outside of the block scope CAN'T override the value in the one living inside (or viceversa)
+      A variable with the same name outside of the block scope CAN'T override the value in the one living inside (or viceversa).
+
     You CAN'T redeclare them (in the same scope).
     You CAN'T reassign CONST, but you CAN reassign LET:
     If CONST is an object or array, you can change the values inside of them because CONST only ensures the reference remains constant. You need Object.freeze() to make the values inside of them inmutable.
-    Declaration is hoisted as VARIABLE NOT INITIALIZED
-*/
+    Declaration is hoisted as VARIABLE NOT INITIALIZED.
 
+    VARIABLE SHADOWING 
+    Several variables with the same names but different values can coexist if:
+     1. They belong to different scopes.
+     2. Variables living in a block scope are declared with const/let.
+    Otherwise, this results in illegal shadowing.
+*/
+console.log("\n===============VAR============");
 console.log(variable1);
 
 if (true) {
@@ -22,6 +29,7 @@ if (true) {
 var variable1 = "Bye";
 console.log(variable1);
 
+console.log("\n===============LET/CONST============");
 if (true) {
   // console.log(variable2); // ReferenceError: Cannot access 'variable2' before initialization
   let variable2 = "Hello";
@@ -29,3 +37,16 @@ if (true) {
 }
 
 // console.log(variable2); //ReferenceError: variable2 is not defined
+
+console.log("\n===============VARIABLE SHADOWING============");
+var a = 2;
+let b = 5;
+
+if (true) {
+  let a = 3;
+  //var b = 0; //ILLEGAL SHADOWING: Cannot redeclared block-scope variable "b"
+  console.log("a:", a);
+  //console.log(b);
+}
+console.log("a:", a);
+console.log("b:", b);
