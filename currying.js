7b4780b90d0d7e1dd77a513aa71bb55fd93e31ea
Renzo Diaz <renzodiaz554@gmail.com>
2024-05-05 10:47:54 -0500

    Currying updated

--- currying.js
+++ currying.js
@@ -1,11 +1,10 @@
 /*
     WHAT IS?
     Transformation of functions with multiple arguments into a sequence of functions each taking one argument at a time.
-    Currying is possible when the curried function has a fixed number of arguments.
     Useful to create partially applied functions.
 */
 
-console.log("==============SIMPLE CURRYING: SUM==============");
+console.log("==============SIMPLE CURRYING==============");
 function simpleCurry(someFunction) {
   return function (a) {
     return function (b) {
@@ -22,13 +21,13 @@ let curriedSum = simpleCurry(sum);
 
 console.log(curriedSum(1)(2));
 
-console.log("==============ADVANCED CURRYING: LOGGING==============");
+console.log("\n==============ADVANCED CURRYING: FIXED NUMBER OF PARAMETERS==============");
 const advancedCurry = (someFunc) => {
   const expectedArgs = someFunc.length;
   // console.log("expectedArgs:", expectedArgs); //Logs once
   const curried = (...args) => {
     // console.log("args:", ...args); //Logs as many times as needed to get args.length >= expectedArgs
-    return args.length >= expectedArgs ? someFunc(...args) : (...args2) => curried(...args.concat(args2));
+    return args.length >= expectedArgs ? someFunc(...args) : (...moreArgs) => curried(...args, ...moreArgs);
   };
   return curried;
 };
@@ -40,11 +39,38 @@ function log(date, importance, message) {
 const curriedLog = advancedCurry(log);
 
 curriedLog(new Date(), "DEBUG", "random debug");
+curriedLog(new Date(), "DEBUG")("random debug");
+curriedLog(new Date())("DEBUG", "random debug");
 curriedLog(new Date())("DEBUG")("random debug");
 
-console.log("==============PARTIALLY APPLIED FUNCTION==============");
+console.log("\n==============PARTIALLY APPLIED FUNCTION==============");
 const logNow = curriedLog(new Date());
 logNow("INFO", "My message");
 
 let debugNow = logNow("DEBUG");
 debugNow("some message");
+
+console.log("\n==============ADVANCED CURRYING: ANY NUMBER OF PARAMETERS==============");
+
+function dynamicCurry(fn, ...args) {
+  const accumulateArgs = (...moreArgs) => {
+    if (moreArgs.length === 0) {
+      return fn(...args);
+    } else {
+      const combinedArgs = args.concat(moreArgs);
+      return dynamicCurry(fn, ...combinedArgs);
+    }
+  };
+  return accumulateArgs;
+}
+
+function multiply(...args) {
+  return args.reduce((acc, val) => acc * val, 1);
+}
+
+console.log(dynamicCurry(multiply, 3)(5)());
+
+const curriedMultiplyByTwo = dynamicCurry(multiply, 2);
+console.log(curriedMultiplyByTwo(5)());
+console.log(curriedMultiplyByTwo(5)(5)());
+console.log(curriedMultiplyByTwo(5, 5)());
