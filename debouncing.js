4ed08c2e30056f6d06241451496b1bb82313d05e
Renzo Diaz <renzodiaz554@gmail.com>
2024-06-04 09:26:11 -0500

    feat: Debouncing and Throttling

---
+++ debouncing.js
@@ -0,0 +1,40 @@
+/*
+  DEBOUNCING
+  Optimization technique to execute a function only when the user stops performing a certain action AND a specific cooling/delay time has passed.
+
+  NOTE: If not used properly, this can introduce latency in the user interface.
+*/
+
+const btn = document.querySelector(".increment_btn");
+const btnPress = document.querySelector(".increment_passed");
+const count = document.querySelector(".increment_count");
+
+let pressedCount = 0;
+let triggerCount = 0;
+
+const myDebouncer = (callback, delay) => {
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+};
+
+const increasePressed = () => {
+  pressedCount += 1;
+  btnPress.innerHTML = pressedCount;
+};
+
+const increaseTriggerCounter = () => {
+  triggerCount += 1;
+  count.innerHTML = triggerCount;
+};
+
+const increaseTrigger = myDebouncer(increaseTriggerCounter, 800);
+
+btn.addEventListener("click", () => {
+  increasePressed();
+  increaseTrigger();
+});
---
+++ throttling.js
@@ -0,0 +1,43 @@
+/*
+  THROTTILING
+  Optimization technique to execute a function once in every specified time interval.
+
+  NOTE: If not used properly, this can introduce loss of information/accuracy in your code.
+*/
+
+const btn = document.querySelector(".increment_btn");
+const btnPress = document.querySelector(".increment_passed");
+const count = document.querySelector(".increment_count");
+
+let pressedCount = 0;
+let triggerCount = 0;
+
+const myThrottle = (callback, delay) => {
+  let isRunning = false;
+  return function (...args) {
+    if (!isRunning) {
+      isRunning = true;
+      callback(...args);
+      setTimeout(() => {
+        isRunning = false;
+      }, delay);
+    }
+  };
+};
+
+const increasePressed = () => {
+  pressedCount += 1;
+  btnPress.innerHTML = pressedCount;
+};
+
+const increaseTriggerCounter = () => {
+  triggerCount += 1;
+  count.innerHTML = triggerCount;
+};
+
+const increaseTrigger = myThrottle(increaseTriggerCounter, 800);
+
+btn.addEventListener("click", () => {
+  increasePressed();
+  increaseTrigger();
+});
